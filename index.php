5584f772b57ce46b2aa85e5e00452b31e8e0111c
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-03-20 21:10:38 +0900

    Shuffle quiz

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once(__DIR__ . '/config.php');
+require_once(__DIR__ . '/Quiz.php');
+
+$quiz = new MyApp\Quiz();
+$data = $quiz->getCurrentQuiz();
+shuffle($data['a']);
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Quiz</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="container">
+      <h1>Q. <?= h($data['q']); ?></h1>
+      <ul>
+        <?php foreach ($data['a'] as $a): ?>
+          <li class="answer"><?= h($a); ?></li>
+        <?php endforeach; ?>
+      </ul>
+      <div id="btn" class="disabled">Next Question</div>
+    </div>
+</body>
+</html>
