b2d16fcd339a1aca95e5734ead812dd9556eaa89
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-03-22 21:10:03 +0900

    Finish lesson

--- Quiz.php
+++ Quiz.php
@@ -9,15 +9,46 @@ class Quiz {
   public function __construct()
   {
     $this->_setup();
+    Token::create();
 
+    // Session dataの初期化
     if (!isset($_SESSION['current_num'])) {
-      $_SESSION['current_num'] = 0;
+      $this->_initSession();
     }
   }
 
+  private function _initSession() {
+    $_SESSION['current_num'] = 0;
+    $_SESSION['current_count'] = 0;
+  }
+
   public function checkAnswer() {
+    Token::validate('token');
     $correctAnswer = $this->_quizSet[$_SESSION['current_num']]['a'][0];
+    if (!isset($_POST['answer'])) {
+      throw new \Exception("answer not set!");
+    }
+    if ($correctAnswer === $_POST['answer']) {
+      $_SESSION['current_count']++;
+    }
+    $_SESSION['current_num']++;
     return $correctAnswer;
+  }
+
+  public function isFinished() {
+    return count($this->_quizSet) === $_SESSION['current_num'];
+  }
+
+  public function getScore() {
+    return round($_SESSION['current_count'] / count($this->_quizSet) * 100);
+  }
+
+  public function isLast() {
+    return count($this->_quizSet) === $_SESSION['current_num'] + 1;
+  }
+
+  public function reset() {
+    $this->_initSession();
   }
 
   public function getCurrentQuiz() {
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 
 require_once(__DIR__ . '/config.php');
-require_once(__DIR__ . '/Quiz.php');
+
 
 $quiz = new MyApp\Quiz();
-$data = $quiz->getCurrentQuiz();
-shuffle($data['a']);
+
+if (!$quiz->isFinished()) {
+  $data = $quiz->getCurrentQuiz();
+  shuffle($data['a']);
+}
 
 ?>
 <!DOCTYPE html>
@@ -16,6 +19,16 @@ shuffle($data['a']);
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+  <?php if ($quiz->isFinished()) : ?>
+    <div id="container">
+      <div id="result">
+        Your score ...
+        <div><?= h($quiz->getScore()); ?> %</div>
+        <a href=''><div id="btn">Replay?</div></a>
+      </div>
+    </div>
+  <?php $quiz->reset(); ?>
+  <?php else : ?>
     <div id="container">
       <h1>Q. <?= h($data['q']); ?></h1>
       <ul>
@@ -23,9 +36,11 @@ shuffle($data['a']);
           <li class="answer"><?= h($a); ?></li>
         <?php endforeach; ?>
       </ul>
-      <div id="btn" class="disabled">Next Question</div>
+      <div id="btn" class="disabled"><?=($quiz->isLast() ? 'Show Result' : 'Next Question'); ?></div>
+      <input type="hidden" id="token" value="<?= h($_SESSION['token']); ?>">
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="quiz.js"></script>
+  <?php endif; ?>
 </body>
 </html>
